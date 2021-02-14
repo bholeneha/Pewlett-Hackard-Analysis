759a82201a37ec341f31f6f6b8baacbd3cf04670
Neha Bhole <nehasbhole@gmail.com>
2021-02-14 00:30:17 -0500

    Uploading queries

--- schema.sql
+++ schema.sql
@@ -50,8 +50,20 @@ CREATE TABLE dept_emp (
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
-    PRIMARY KEY (dept_no, emp_no)
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
 SELECT * FROM departments;
+
+DROP TABLE dept_emp;
+
+CREATE TABLE dept_emp (
+	emp_no INT NOT NULL,
+	dept_no VARCHAR(4) NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
+);
+
+SELECT * FROM dept_emp;
